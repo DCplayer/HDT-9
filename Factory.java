7a36573a85edd71d469f92b56890e859fbb37ec7
CoffeeBrewer <cas15151@uvg.edu.gt>
2016-10-16 17:45:41 -0600

    Creando Implementaciones
    Se crearon las implementaciones de HashMap y SplayTree a utilizar. El
    SplayTree es codigo que se tomo del programador Josh Israel.

--- Factory.java
+++ Factory.java
@@ -5,6 +5,8 @@
  * @author Rafael de Leon 13361
  * @author Diego Castaneda 15151
  */
+
+
 public class Factory {
     public static final String MAPA = "MAPA";
     public static final String ARBOL = "ARBOL";
@@ -12,15 +14,14 @@ public class Factory {
     /**
      *@param implementacion parametro que dicta que implementacion se utilizara
      */
-    public static AbstractFactory getFactory(String implementacion){
+    public static Object getFactory(String implementacion){
         if(implementacion.equalsIgnoreCase(MAPA)){
-            return new MapDictionary();
+            return new MapDictionary(7500);
         }
         if(implementacion.equalsIgnoreCase(ARBOL)){
-            return new TreeDictionary();
+            return new SplayBST<String,String>();
         }
         return null;
     }
 }
 
-}
---
+++ MapDictionary.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+/**
+ * MapDictionary es la implementacion para el diccionario con mapeo
+ *
+ * @author Julio Barahona 141206
+ * @author Rafael de Leon 13361
+ * @author Diego Castaneda 15151
+ */
+public class MapDictionary extends HashMap{
+
+    public MapDictionary(int initialCapacity) {
+        super(initialCapacity);
+    }
+}
